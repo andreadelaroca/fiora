b524672a01b9be7a4fd9f63bb850110c4887f1a5
KDCar15 <caaguirre@uamv.edu.ni>
2025-11-19 09:24:43 -0600

    Clase 'Inicio de sesión' añadida

---
+++ Fiora/Models/InicioSesion.cs
@@ -0,0 +1,30 @@
+using Fiora.Models;
+
+public class InicioSesion
+{
+    // To do: Añadir toda la clase cuando no tengás que estar ocupada con otras clases.
+    private string nombre_usuario = null;
+    private bool autenticado = false;
+    private int id_usuario = 0;
+    private int tipo_usuario = 0;
+    private int ultimo_acceso = 0;
+    public string ver_nombre_usuario() { return this.nombre_usuario; }
+
+    public void iniciar_sesion()
+    {
+    
+    }
+    public void cerrar_sesion()
+    {
+    
+    }
+    public void validar_credenciales()
+    {
+    
+    }
+    public void obtener_datos()
+    {
+
+    }
+
+}
