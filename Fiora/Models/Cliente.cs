955b07a4c1cdc384ce4bc9e5f3823907d598e7e0
andreadelaroca <asodelaroca@uamv.edu.ni>
2025-11-18 20:44:34 -0600

    Atributos de clase Cliente

--- Fiora/Models/Cliente.cs
+++ Fiora/Models/Cliente.cs
@@ -3,7 +3,12 @@
     public class Cliente
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string NombreCliente { get; set; }
+        public string CorreoCliente { get; set; }
+        public string TelefonoCliente { get; set; }
+
+        public int List<Pedido> Pedidos { get; set; } // Relación uno a muchos con la entidad Pedido
+        // TODO arreglar esta lista
 
         public Cliente()
         {
