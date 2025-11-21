1557c6e72e6873bc6ef8a4d3680e68015ba2f254
sar24001 <masarante@uamv.edu.ni>
2025-11-21 09:49:29 -0600

    Creacion del index y catalogo

---
+++ Fiora/Controllers/CatalogoController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Fiora.Controllers
+{
+    public class CatalogoController : Controller
+    {
+        // Acción principal del catálogo
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
