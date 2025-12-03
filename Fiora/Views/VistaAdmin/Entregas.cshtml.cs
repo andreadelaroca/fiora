210d315eb1a6cfa56d24dccbecf2146e74eddf72
KDCar15 <caaguirre@uamv.edu.ni>
2025-12-02 22:24:46 -0600

    Correcciones menores

---
+++ Fiora/Views/VistaAdmin/Entregas.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Fiora.Views.VistaAdmin
+{
+    public class EntregasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
